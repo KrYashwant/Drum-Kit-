e1a250868d38b851f47e66b161322c710f7b095b
KrYashwant <72462067+KrYashwant@users.noreply.github.com>
2021-09-06 15:32:04 +0530

    Add files via upload

---
+++ Drum Kit/index.js
@@ -0,0 +1,38 @@
+document.querySelector(".w.drum").addEventListener("click",function(){
+    var audio = new Audio('sounds/crash.mp3');
+     audio.play();
+    this.style.color="white";
+     
+});
+document.querySelector(".a.drum").addEventListener("click",function(){
+    var audio = new Audio('sounds/kick-bass.mp3');
+    audio.play();
+    this.style.color="white";
+});
+document.querySelector(".s.drum").addEventListener("click",function(){
+    var audio = new Audio('sounds/snare.mp3');
+    audio.play();
+    this.style.color="white";
+});
+document.querySelector(".d.drum").addEventListener("click",function(){
+    var audio = new Audio('sounds/tom-1.mp3');
+    audio.play();
+    this.style.color="white";
+});
+document.querySelector(".j.drum").addEventListener("click",function(){
+    var audio = new Audio('sounds/tom-2.mp3');
+    audio.play();
+    this.style.color="white";
+});
+document.querySelector(".k.drum").addEventListener("click",function(){
+    var audio = new Audio('sounds/tom-3.mp3');
+    audio.play();
+    this.style.color="white";
+});
+document.querySelector(".l.drum").addEventListener("click",function(){
+    var audio = new Audio('sounds/tom-4.mp3');
+    audio.play();
+    this.style.color="white";
+});
+
+    
